7d71f5b176a00b6f1c0b00818b47d7b23160b8e1
dhvudh <105971173+dhvudh@users.noreply.github.com>
2024-04-07 14:32:19 +0530

    Update Nopage.jsx

--- src/pages/nopage/Nopage.jsx
+++ src/pages/nopage/Nopage.jsx
@@ -5,7 +5,7 @@ function Nopage() {
   return (
     <Layout>
       <div className='flex justify-center items-center h-screen overflow-hidden'>
-        <img src='https://img.freepik.com/premium-vector/404-error-page-banner-with-found-text-cute-dog-background-error-404-concept-web-design-element_253396-497.jpg'/>
+        <img src='https://img.freepik.com/premium-vector/404-error-page-banner-with-found-text-cute-dog-background-error-404-concept-web-design-element_253396-497.jpg' alt="404"/>
       </div>
     </Layout>
   )
